a8ff2d82ac702c4d4e21dd369c9d86e000ff06cc
James Su-Shum <sushumjames@gmail.com>
2020-09-30 16:39:39 -0400

    Added BaseRepository, PostRepository and Interface, and DBUtils class

---
+++ Gifter/Repositories/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Repositories
+{
+    // Abstract keyword indicates that BaseRepository cannot be directly instantiated, but can only be used by inheritance
+    public abstract class BaseRepository
+    {
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
